e8d24a1bfa589e329bedf03d02af421413b52286
theripper93 <tsplab@gmail.com>
2023-05-30 18:10:58 +0200

    a

--- scripts/systems.js
+++ scripts/systems.js
@@ -1,6 +1,6 @@
 export function defaultAttributesConfig() {
     return {
-        "dnd5e": [
+        dnd5e: [
             {
                 attr: "attributes.hp.value",
                 icon: "fa-solid fa-heart",
@@ -20,7 +20,7 @@ export function defaultAttributesConfig() {
                 attr: "attributes.spelldc",
                 icon: "fa-solid fa-hand-holding-magic",
                 units: "Spell DC",
-            }
+            },
         ],
         "cyberpunk-red-core": [
             {
@@ -53,29 +53,35 @@ export function defaultAttributesConfig() {
             {
                 attr: "wildcard",
                 icon: "fa-solid fa-cards",
+                units: `(${game.i18n.localize("SWADE.WildCard")})`,
             },
             {
-                attr: "wounds.value",
-                icon: "fa-solid fa-heart",
+            attr: "wounds.value",
+            icon: "fa-solid fa-heart",
+            units: `(${game.i18n.localize("SWADE.Wounds")})`,
             },
             {
                 attr: "fatigue.value",
                 icon: "fa-solid fa-face-hand-yawn",
+                units: `(${game.i18n.localize("SWADE.Fatigue")})`,
             },
             {
                 attr: "stats.speed.value",
                 icon: "fa-solid fa-person-running",
+                units: `(${game.i18n.localize("SWADE.Pace")})`,
             },
             {
                 attr: "stats.toughness.value",
                 icon: "fa-solid fa-shield",
+                units: `(${game.i18n.localize("SWADE.Tough")})`,
             },
             {
                 attr: "stats.parry.value",
                 icon: "fa-solid fa-swords",
+                units: `(${game.i18n.localize("SWADE.Parry")})`,
             },
         ],
-    }
+    };
 }
 
 export function generateDescription(actor) {
@@ -85,14 +91,20 @@ export function generateDescription(actor) {
             const isPC = actor.type === "character";
             if (isNPC) {
                 const creatureType = game.i18n.localize(CONFIG.DND5E.creatureTypes[actor.system.details.type.value] ?? actor.system.details.type.custom);
-                const cr = actor.system.details.cr >= 1 ? actor.system.details.cr : `1/${1/actor.system.details.cr}`
-                return `CR ${cr} ${creatureType}`
+                const cr = actor.system.details.cr >= 1 ? actor.system.details.cr : `1/${1 / actor.system.details.cr}`;
+                return `CR ${cr} ${creatureType}`;
             } else if (isPC) {
-                const classes = Object.values(actor.classes).map(c => c.name).join(" / ");
-                return `Level ${actor.system.details.level} ${classes} (${actor.system.details.race})`
+                const classes = Object.values(actor.classes)
+                    .map((c) => c.name)
+                    .join(" / ");
+                return `Level ${actor.system.details.level} ${classes} (${actor.system.details.race})`;
             } else {
                 return null;
             }
+        case "swade":
+            if (actor.type !== "character") return;
+            const { system } = actor;
+            return `${system.details.archetype} ${system.details.species.name}, ${game.i18n.localize("SWADE.Rank")}: ${system.advances.rank}`;
     }
 }
 
@@ -102,9 +114,9 @@ export function getInitiativeDisplay(combatant) {
             let suit = "";
             const useImg = true;
             const getCardImage = (cardstr) => {
-                if(!useImg) return null;
-                return Array.from(game.cards.get(game.settings.get('swade', 'actionDeck')).cards).find(c => c.description === cardstr)?.img;
-            }
+                if (!useImg) return null;
+                return Array.from(game.cards.get(game.settings.get("swade", "actionDeck")).cards).find((c) => c.description === cardstr)?.img;
+            };
             const cardString = combatant?.cardString ?? "";
             if (cardString.includes("♥")) suit = "fa-solid fa-heart";
             if (cardString.includes("♦")) suit = "fa-solid fa-diamond";
@@ -115,13 +127,13 @@ export function getInitiativeDisplay(combatant) {
                 value: useImg ? cardString : cardString.replace("♥", "").replace("♦", "").replace("♣", "").replace("♠", ""),
                 icon: getCardImage(cardString) ?? suit,
                 rollIcon: "fa-regular fa-cards-blank",
-            } 
+            };
         }
         default:
             return {
                 value: combatant?.initiative,
                 icon: "fa-regular fa-dice-d20",
                 rollIcon: "fa-regular fa-dice-d20",
-            }
+            };
     }
 }
