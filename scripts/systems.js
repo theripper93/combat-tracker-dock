c39b07ffcdf427a08e8c8560bcaf077b9714aed7
Samuel Grave <samuel@dinie.com>
2023-05-30 12:37:19 -0300

    :recycle: refactor getSwadeAttributes, add localized units and creates generateSwadeDescription

--- scripts/systems.js
+++ scripts/systems.js
@@ -49,32 +49,7 @@ export function defaultAttributesConfig() {
                 units: "SP",
             },
         ],
-        swade: [
-            {
-                attr: "wildcard",
-                icon: "fa-solid fa-cards",
-            },
-            {
-                attr: "wounds.value",
-                icon: "fa-solid fa-heart",
-            },
-            {
-                attr: "fatigue.value",
-                icon: "fa-solid fa-face-hand-yawn",
-            },
-            {
-                attr: "stats.speed.value",
-                icon: "fa-solid fa-person-running",
-            },
-            {
-                attr: "stats.toughness.value",
-                icon: "fa-solid fa-shield",
-            },
-            {
-                attr: "stats.parry.value",
-                icon: "fa-solid fa-swords",
-            },
-        ],
+        "swade": _getSwadeAttributes(),
     }
 }
 
@@ -93,6 +68,8 @@ export function generateDescription(actor) {
             } else {
                 return null;
             }
+        case "swade":
+            return _generateSwadeDescription(actor)
     }
 }
 
@@ -115,7 +92,7 @@ export function getInitiativeDisplay(combatant) {
                 value: useImg ? cardString : cardString.replace("♥", "").replace("♦", "").replace("♣", "").replace("♠", ""),
                 icon: getCardImage(cardString) ?? suit,
                 rollIcon: "fa-regular fa-cards-blank",
-            } 
+            }
         }
         default:
             return {
@@ -124,4 +101,47 @@ export function getInitiativeDisplay(combatant) {
                 rollIcon: "fa-regular fa-dice-d20",
             }
     }
+}
+
+function _getSwadeAttributes() {
+    return [
+        {
+            attr: "wildcard",
+            icon: "fa-solid fa-cards",
+            units: `(${game.i18n.localize("SWADE.WildCard")})`,
+        },
+        {
+        attr: "wounds.value",
+        icon: "fa-solid fa-heart",
+        units: `(${game.i18n.localize("SWADE.Wounds")})`,
+        },
+        {
+            attr: "fatigue.value",
+            icon: "fa-solid fa-face-hand-yawn",
+            units: `(${game.i18n.localize("SWADE.Fatigue")})`,
+        },
+        {
+            attr: "stats.speed.value",
+            icon: "fa-solid fa-person-running",
+            units: `(${game.i18n.localize("SWADE.Pace")})`,
+        },
+        {
+            attr: "stats.toughness.value",
+            icon: "fa-solid fa-shield",
+            units: `(${game.i18n.localize("SWADE.Tough")})`,
+        },
+        {
+            attr: "stats.parry.value",
+            icon: "fa-solid fa-swords",
+            units: `(${game.i18n.localize("SWADE.Parry")})`,
+        },
+    ]
+}
+
+function _generateSwadeDescription(actor) {
+    if (actor.type !== "character") return
+    const { system } = actor;
+    return `${system.details.archetype} ${
+        system.details.species.name
+    }, ${game.i18n.localize("SWADE.Rank")}: ${system.advances.rank}`
 }
